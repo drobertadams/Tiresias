8fb6d3eb1102cac1d6a874bad2c1d718c17690d3
Robert Adams <d.robert.adams@gmail.com>
2017-02-23 16:48:39 -0500

    Forgot to whitelist the new :role parameter in User.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(
-        :email, :password, :password_confirmation
+        :email, :password, :password_confirmation, :role
       )
     end
 
